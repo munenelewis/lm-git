7509ab8b84f98dabfff83ce314848f4d565b2808
lewis <lm@m-power.io>
2021-06-09 12:15:01 +0300

    add a fuction to handle repo directory without creating an new file

--- libwyag.py
+++ libwyag.py
@@ -104,4 +104,25 @@ def repo_path(repo, path):
     return os.path.join(repo.gitdir, *path)
 
 
+def repo_file(repo, *path, mkdir=False):
+    """ creates files like .git/refs/remotes/origin from  repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\")"""
 
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir."""
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception("Not a directory: %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+
+    else:
+        return None
