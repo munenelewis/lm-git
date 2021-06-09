1e29de939d0fe07cc87ceaf971dcc4e6532215c1
lewis <lm@m-power.io>
2021-06-09 09:45:31 +0300

    add imports we need and the wyag file

---
+++ libwyag.py
@@ -0,0 +1,12 @@
+#we will use argparse to be able to pass our commands in terminal 
+import argparse
+
+# We’ll need a few more container types than the base lib provides, most notably an OrderedDict
+import collections
+# Git uses a configuration file format that is basically Microsoft’s INI format. The configparser module can read and write these files
+import configparser
+import configparser
+import os
+import re
+import sys
+import zlib
---
+++ readMe.md
@@ -0,0 +1,3 @@
+## i have been curious about how git works so i decided to build a git like feature using python 
+
+# i.e in no way will this reach the leave of complexity git had , ill just archive the core concepts of git i.e init , log merge etc 
