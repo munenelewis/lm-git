ca806e3f1e3fb67943b88fd728567c70ebe79345
lewis <lm@m-power.io>
2021-06-09 11:51:33 +0300

    add a git repository object

--- libwyag.py
+++ libwyag.py
@@ -29,6 +29,10 @@ argparser = argparse.ArgumentParser(description="Content tracker")
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
 
+'''
+    we  read this string and call the correct function accordingly
+'''
+
 
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
@@ -61,3 +65,35 @@ def main(argv=sys.argv[1:]):
         cmd_show_ref(args)
     elif args.command == "tag":
         cmd_tag(args)
+
+
+class GitRepository(object):
+    """
+        A git repository
+    """
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not(force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a Git repository %S" % path)
+
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+
+        else if not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception(
+                    "Unsupported repositoryformatversion %s" % vers)
