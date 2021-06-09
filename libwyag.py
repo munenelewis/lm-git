f7f4c2f89879a1bfb117cbee68616dfefbe6e672
lewis <lm@m-power.io>
2021-06-09 11:41:21 +0300

    add some basic comands to be used

--- libwyag.py
+++ libwyag.py
@@ -1,12 +1,63 @@
-#we will use argparse to be able to pass our commands in terminal 
+# we will use argparse to be able to pass our commands in terminal
 import argparse
-
 # We’ll need a few more container types than the base lib provides, most notably an OrderedDict
 import collections
+
 # Git uses a configuration file format that is basically Microsoft’s INI format. The configparser module can read and write these files
 import configparser
-import configparser
+
+# Git uses the SHA-1 function quite extensively. In Python, it’s in hashlib
+import hashlib
+
+# filesystem abstraction routines
 import os
+
+# regular expressions
 import re
+
+# sys to access the actual command-line arguments
 import sys
+
+# compresses everything using zlib
 import zlib
+
+
+argparser = argparse.ArgumentParser(description="Content tracker")
+'''
+    this ensures you just dont call git , we need to call git [argument] i.e git init 
+'''
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
+
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+
+    if args.command == 'add':
+        cmd_add(args)
+    elif args.command == "cat-file":
+        cmd_cat_file(args)
+    elif args.command == "checkout":
+        cmd_checkout(args)
+    elif args.command == "commit":
+        cmd_commit(args)
+    elif args.command == "hash-object":
+        cmd_hash_object(args)
+    elif args.command == "init":
+        cmd_init(args)
+    elif args.command == "log":
+        cmd_log(args)
+    elif args.command == "ls-tree":
+        cmd_ls_tree(args)
+    elif args.command == "merge":
+        cmd_merge(args)
+    elif args.command == "rebase":
+        cmd_rebase(args)
+    elif args.command == "rev-parse":
+        cmd_rev_parse(args)
+    elif args.command == "rm":
+        cmd_rm(args)
+    elif args.command == "show-ref":
+        cmd_show_ref(args)
+    elif args.command == "tag":
+        cmd_tag(args)
